00d03c818a184e5909f7418290a1902c903f05c4
dikey <DikeyWang@163.com>
2019-12-21 20:39:42 +0800

    2019-12-21

--- src/main/java/yzspring/beans/spport/BeanDefinitionReader.java
+++ src/main/java/yzspring/beans/spport/BeanDefinitionReader.java
@@ -1,14 +1,7 @@
 package yzspring.beans.spport;
 
-import jdk.nashorn.internal.runtime.arrays.ContinuousArrayData;
-import yzspring.beans.config.BeanDefinition;
-
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 /**
@@ -23,47 +16,22 @@ public class BeanDefinitionReader {
 
     private Properties properties = new Properties();
 
-    /**保存了所有Bean的className*/
-    private List<String> registyBeanClasses = new ArrayList<>();
-
     public BeanDefinitionReader(String... locations) {
-
-        //1.定位，通过URL定位找到配置文件，然后转换为文件流
-        InputStream is = this.getClass().getClassLoader().getResourceAsStream(locations[0].replace("classpath:", ""));
         try {
-            //2.加载，保存为properties
-            properties.load(is);
-        } catch (IOException e) {
+            //定位
+            InputStream inputStream = this.getClass().getResourceAsStream(locations[0].replace("classpath:",""));
+            //导入
+            properties.load(inputStream);
+        }catch (IOException e){
             e.printStackTrace();
         }
-
-        //3.扫描，扫描资源文件(class)，并保存到集合中
         doScanner(properties.getProperty(MY_SPRING_SCAN_PACKAGE));
     }
 
     private void doScanner(String property) {
-        URL url = this.getClass().getClassLoader().getResource(property.replaceAll("\\.", "/"));
-        File classPath = new File(url.getFile());
-        for (File file : classPath.listFiles()) {
-            if (file.isDirectory()) {
-                //如果是目录则递归调用，直到找到class
-                doScanner(property + "." + file.getName());
-            } else {
-                if (!file.getName().endsWith(".class")) {
-                    continue;
-                }
-                String className = (property + "." + file.getName().replace(".class", ""));
-                //className保存到集合
-                registyBeanClasses.add(className);
-            }
-        }
     }
 
-    public Properties getProperties() {
+    public Properties getProperties(){
         return properties;
-    }
-
-    public List<BeanDefinition> loadBeanDefinitions() {
-        return null ;
     }
 }
--- src/main/java/yzspring/context/spport/DefaultApplicationContext.java
+++ src/main/java/yzspring/context/spport/DefaultApplicationContext.java
@@ -1,11 +1,7 @@
 package yzspring.context.spport;
 
-import yzspring.beans.config.BeanDefinition;
 import yzspring.beans.spport.BeanDefinitionReader;
 import yzspring.context.ApplicationContext;
-
-import java.io.FilterReader;
-import java.util.List;
 
 /**
  * 类描述
@@ -17,8 +13,9 @@ public class DefaultApplicationContext implements ApplicationContext{
 
     //配置文件路径
     private String configLocaltion;
+
     //reader
-    BeanDefinitionReader reader;
+    private BeanDefinitionReader reader;
 
     public DefaultApplicationContext(String configLocaltion) {
         this.configLocaltion = configLocaltion;
@@ -33,9 +30,9 @@ public class DefaultApplicationContext implements ApplicationContext{
     private void refresh() throws Exception{
 
         //1、定位，定位配置文件
-         reader = new BeanDefinitionReader(this.configLocaltion);
+        reader = new BeanDefinitionReader(configLocaltion);
         //2、加载配置文件，扫描相关的类，把它们封装成BeanDefinition
-        List<BeanDefinition> beanDefinitions = reader.loadBeanDefinitions();
+
         //3、注册，把配置信息放到容器里面(伪IOC容器)
         //到这里为止，容器初始化完毕
 
